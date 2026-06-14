bfcf8e4ce764b962e6469f632d01b20f006fa196
zinmarhnin818-coder <zinmarhnin818@gmail.com>
2026-06-15 00:09:33 +0900

    ユーザー編集機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_13_184508) do
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
